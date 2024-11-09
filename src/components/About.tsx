bfbfb5e8aa9306a0ef74369134e671fba253f4e1
muhammadramzan2803 <noureen.rafiq2@gmail.com>
2024-11-09 16:57:19 +0500

    first commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,10 +5,10 @@ const About = () => {
     <div id='about' className='container pt-32'>
       <h2 className='text-4xl md:text-5xl' data-aos="zoom-in-up">About Me</h2>
       <p className='text-gray-500 pt-4' data-aos="zoom-in-up">
-        I'm a software engineer with a passion for creating user-friendly experiences. I've been working remotely for the past five years, focusing primarily on web development and backend development. I'm currently based in New York City.
-        I enjoy collaborating with teams to design and develop innovative solutions. I'm also always looking for opportunities to learn new technologies and improve my skills.
+        I&apos;m a software engineer with a passion for creating user-friendly experiences. I&apos;ve been working remotely for the past five years, focusing primarily on web development and backend development. I&apos;m currently based in New York City.
+        I enjoy collaborating with teams to design and develop innovative solutions. I&apos;m also always looking for opportunities to learn new technologies and improve my skills.
         Outside of work, I enjoy playing soccer, cooking, and exploring the city. I find solace in the simple pleasures of life and the connections I build through my work.
-        I'm always open to discussing opportunities to work together, so don't hesitate to reach out.
+        I&apos;m always open to discussing opportunities to work together, so don&apos;t hesitate to reach out.
         Thank you for taking the time to read about me!
       </p>
     </div>
